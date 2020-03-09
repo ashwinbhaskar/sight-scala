37459fa7aee223fc760d9f15d27fbfdf4d28eea7
Ashwin Bhaskar <ashwin.bhaskar@go-jek.com>
2020-03-09 09:45:41 +0530

    Add Pages

--- src/main/scala/Models.scala
+++ src/main/scala/Models.scala
@@ -5,3 +5,5 @@ case class RecognizedText(text: String, confidence: String, topLeftX: Int, topLe
 
 case class Page(error: Option[String], fileIndex: Int, pageNumber: Int, numberOfPagesInFile: Int
 ,recognizedText: Seq[RecognizedText])
+
+case class Pages(page: Seq[Page])
