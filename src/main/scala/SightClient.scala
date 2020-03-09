9565c44d786e253a7fe257e2f1d4318ade855ecb
Ashwin Bhaskar <ashwin.bhaskar@go-jek.com>
2020-03-09 09:45:59 +0530

    Add APIs

--- src/main/scala/SightClient.scala
+++ src/main/scala/SightClient.scala
@@ -1,6 +1,8 @@
 package sightapi
 
 import types.APIKey
+import models.Pages
 
 class SightClient(private val apiKey: APIKey)
-    def recognizePayload(filePaths: Seq[String]) = ???
+    def recognize(filePaths: Seq[String]): Either[Error,Pages] = recognize(filePaths, false)
+    def recognize(filePaths: Seq[String], shouldWordLevelBoundBoxes: Boolean): Either[Error, Pages] = ???
