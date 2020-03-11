caae07510b17a81e77ee04c36f146a054e55b0ea
Ashwin Bhaskar <ashwin.bhaskar@go-jek.com>
2020-03-11 13:41:00 +0530

    Add encoding and decoding tests for Page

--- src/test/scala/JsonTest.scala
+++ src/test/scala/JsonTest.scala
@@ -1,12 +1,13 @@
 import io.circe.{Encoder, Decoder}
-import models.RecognizedText
-import givens.{given Encoder[RecognizedText],given Decoder[RecognizedText]}
+import models.{RecognizedText, Page}
 import io.circe.syntax.{given _}
 import io.circe.Json
 import munit.Location.generate
 
 
 class JsonTest extends munit.FunSuite
+
+    import givens.{given Encoder[RecognizedText],given Decoder[RecognizedText]}
     test("Should encode and decode recognized text json correctly") {
         val recognizedText = RecognizedText("foo-text", "foo-confidence", 1, 2, 3, 4, 5, 6, 7, 8)
         val expected: Json = Json.obj(
@@ -27,4 +28,57 @@ class JsonTest extends munit.FunSuite
             case Left(failure) => assertFail(s"failed with $failure")
             case Right(decoded) => assertEquals(decoded, recognizedText)
     }
+
+    import givens.{given Encoder[Page], given Decoder[Page]}
+    test("Should encode and decode page json correctly when Error is None") {
+        val recognizedText = RecognizedText("foo-text", "foo-confidence", 1, 2, 3, 4, 5, 6, 7, 8)
+        val page: Page = Page(None, 0, 1, 3, Seq(recognizedText))
+        val expected: Json = Json.obj(
+            ("Error", Json.Null),
+            ("FileIndex", Json.fromInt(0)),
+            ("PageNumber", Json.fromInt(1)),
+            ("NumberOfPagesInFile", Json.fromInt(3)),
+            ("RecognizedText", page.recognizedText.asJson)
+        )
+        val actual: Json = page.asJson
+        assertEquals(actual, expected)
+        expected.as[Page] match
+            case Left(failure) => assertFail(s"failed with $failure")
+            case Right(decoded) => assertEquals(decoded, page)
+    }
+    test("Should encode and decode page json correctly when Error is NOT None but IS empty") {
+        val recognizedText = RecognizedText("foo-text", "foo-confidence", 1, 2, 3, 4, 5, 6, 7, 8)
+        val page: Page = Page(Some(""), 0, 1, 3, Seq(recognizedText))
+        val expected: Json = Json.obj(
+            ("Error", Json.Null),
+            ("FileIndex", Json.fromInt(0)),
+            ("PageNumber", Json.fromInt(1)),
+            ("NumberOfPagesInFile", Json.fromInt(3)),
+            ("RecognizedText", page.recognizedText.asJson)
+        )
+        val actual: Json = page.asJson
+        assertEquals(actual, expected)
+        expected.as[Page] match
+            case Left(failure) => assertFail(s"failed with $failure")
+            case Right(decoded) => assertEquals(decoded, page.copy(error = None))
+    }
+    test("Should encode and decode page json correctly when Error is NOT empty") {
+        val recognizedText = RecognizedText("foo-text", "foo-confidence", 1, 2, 3, 4, 5, 6, 7, 8)
+        val page: Page = Page(Some("There was an error"), 0, 1, 3, Seq(recognizedText))
+        val expected: Json = Json.obj(
+            ("Error", Json.fromString("There was an error")),
+            ("FileIndex", Json.fromInt(0)),
+            ("PageNumber", Json.fromInt(1)),
+            ("NumberOfPagesInFile", Json.fromInt(3)),
+            ("RecognizedText", page.recognizedText.asJson)
+        )
+        val actual: Json = page.asJson
+        assertEquals(actual, expected)
+        expected.as[Page] match
+            case Left(failure) => assertFail(s"failed with $failure")
+            case Right(decoded) => assertEquals(decoded, page)
+    }
+
+    
         
+
