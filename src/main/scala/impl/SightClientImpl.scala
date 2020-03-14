7b90e4385798e682d50afb16d941f6cb1bd97faf
Ashwin Bhaskar <ashwin.bhaskar@go-jek.com>
2020-03-14 09:47:41 +0530

    Complete SightClientImpl

--- src/main/scala/impl/SightClientImpl.scala
+++ src/main/scala/impl/SightClientImpl.scala
@@ -16,8 +16,9 @@ import givens.{given Decoder[Pages], given Decoder[RecognizedTexts], given Decod
 import sttp.client.circe.circeBodySerializer
 import cats.implicits.{given _}
 import scala.language.implicitConversions
+import sttp.client.{SttpBackend, Identity, NothingT}
 
-class SightClientImpl(private val apiKey: APIKey, private val fileContentReader: FileContentReader) extends SightClient(apiKey, fileContentReader)
+class SightClientImpl(private val apiKey: APIKey, private val fileContentReader: FileContentReader)(using SttpBackend[Identity, Nothing, NothingT]) extends SightClient(apiKey, fileContentReader)
     private case class FileContent(mimeType: MimeType, base64FileContent: String)
     private case class Payload(shouldMakeSentences: Boolean, files: Seq[FileContent])
     private given Encoder[FileContent]
@@ -29,7 +30,29 @@ class SightClientImpl(private val apiKey: APIKey, private val fileContentReader:
             ("makeSentences", Json.fromBoolean(payload.shouldMakeSentences)),
             ("files", payload.files.asJson))
     
-    private def handlePollingUrl(url: String, numberOfFiles: Int): Either[Error, Pages] = ???
+    //side effecty function
+    private def markSeen(pageSeenTracker: Array[Array[Boolean]], pages: Seq[Page]): Unit = 
+        pages.filter(_.pageNumber >= 0).foreach{ page => 
+            if(pageSeenTracker(page.fileIndex).size != page.numberOfPagesInFile)
+                pageSeenTracker(page.fileIndex) = Array.fill(page.numberOfPagesInFile)(false)
+            pageSeenTracker(page.fileIndex)(page.pageNumber) = true
+        }
+
+    private def handlePollingUrl(url: String, numberOfFiles: Int): Either[Error, Pages] = 
+        var pages = List[Page]()
+        val pageSeenTracker: Array[Array[Boolean]] = Array.fill(numberOfFiles)(Array(false))
+        def decodePages(p: String): Either[Error, Pages] = decode[Pages](p).left.map(e => ErrorResponse(e.toString))
+        var error: Option[Error] = None
+        while(error.isEmpty && pageSeenTracker.forall(_.foldLeft(true)(_ && _)))
+            val request = basicRequest.header("Authorization", s"Basic $apiKey").get(uri"$url")
+            val response = request.send()
+            response.body.fold[Either[Error, Pages]](fa = ErrorResponse(_).asLeft[Pages], fb = decodePages) match
+                case Left(err) => error = Some(err)
+                case Right(p) => pages = pages ++ p.pages
+                    markSeen(pageSeenTracker, p.pages)
+        
+        error.fold(Pages(pages).asRight[Error])(_.asLeft[Pages])
+
     
     private def decodeResponse(response: String, numberOfFiles: Int): Either[Error, Pages] = 
         def decodePollingUrl(r: String): Either[Error, PollingUrl] = 
@@ -48,7 +71,7 @@ class SightClientImpl(private val apiKey: APIKey, private val fileContentReader:
     override def recognize(filePaths: Seq[String], shouldWordLevelBoundBoxes: Boolean): Either[Error, Pages] = 
         val payloadOrError: Either[Error, Payload] =  
         for 
-            filePaths1 <- Right(filePaths): Either[Nothing, Seq[String]]
+            filePaths1 <- filePaths.asRight[Nothing]
             mimeTypes <- fileContentReader.fileMimeTypes(filePaths1)
             base64 <- fileContentReader.filesToBase64(filePaths1)
         yield 
@@ -59,6 +82,5 @@ class SightClientImpl(private val apiKey: APIKey, private val fileContentReader:
             case Right(payload) => 
                 val request = basicRequest.body(payload).header("Authorization", s"Basic $apiKey")
                     .post(uri"https://siftrics.com/api/sight/")
-                implicit val backend = HttpURLConnectionBackend()
                 val response = request.send()
-                response.body.fold[Either[Error, Pages]](fa = e => ErrorResponse(e).asLeft[Pages], fb = decodeResponse(_, filePaths.length))
+                response.body.fold[Either[Error, Pages]](fa = ErrorResponse(_).asLeft[Pages], fb = decodeResponse(_, filePaths.length))
