cd771b8e71be1bc22216f08bcec6d776a151903d
Ashwin Bhaskar <ashwin.bhaskar@go-jek.com>
2020-03-14 21:07:25 +0530

    Test HTTP call

--- src/test/scala/SighClientImplTest.scala
+++ src/test/scala/SighClientImplTest.scala
@@ -1,17 +1,50 @@
 import munit.FunSuite
-import models.MimeType
+import models.{MimeType,RecognizedText, RecognizedTexts, Page, Pages}
 import models.MimeType._
 import contracts.FileContentReader
 import models.Error._
 import models.Error
 import types.APIKey
 import impl.SightClientImpl
-import sttp.client.{SttpBackend, Identity, NothingT}
+import sttp.client.{SttpBackend, Identity, NothingT, Request, Response, ResponseAs, StringBody}
 import sttp.client.HttpURLConnectionBackend
+import sttp.model.Method.{GET, POST}
+import sttp.model.{StatusCode, Header}
+import sttp.client.ws.WebSocketResponse
+import sttp.client.monad.{IdMonad, MonadError}
+import io.circe.{Encoder, Decoder}
+import io.circe.syntax.{given _}
+import scala.collection.mutable.ListBuffer
 
 class SightClientImplTest extends FunSuite
 
+    private val postArgs: ListBuffer[(String, String, String)] = ListBuffer() //Url, payload, Auth
+
+    private val getArgs: ListBuffer[(String, String)] = ListBuffer() //url, Auth
+    override def beforeEach(context: BeforeEach): Unit = 
+        postArgs.clear
+        getArgs.clear
+
+
+
     given SttpBackend[Identity, Nothing, NothingT] = HttpURLConnectionBackend()
+    def withSttpBackend[M](postResponse: Either[String, String], getResponse: Either[String, String]) = new SttpBackend[Identity, Nothing, NothingT] with
+        def send[T](request: Request[T, Nothing]): Response[T] = 
+            if(request.method == POST) 
+                val uri = request.productElement(1)
+                val body: StringBody = request.productElement(2).asInstanceOf[StringBody]
+                val payload = body.productElement(0)
+                val headers: Vector[Header] = request.productElement(3).asInstanceOf[Vector[Header]]
+                val authorization = headers(2)
+                postArgs.append((uri.toString, payload.toString, authorization.toString))
+                Response(body = postResponse.asInstanceOf[T],code = StatusCode.Ok)
+            else if(request.method == GET) Response(body = getResponse.asInstanceOf[T], code = StatusCode.Ok)
+            else throw Exception(s"Unexpected post method ${request.method}")
+        def openWebsocket[T, WS_RESULT](request: Request[T, Nothing], handler: NothingT[WS_RESULT]): Identity[WebSocketResponse[WS_RESULT]] = ???
+        def close(): Identity[Unit] = ()
+        def responseMonad: MonadError[Identity] = IdMonad
+
+    
     private val apiKey: APIKey = APIKey("12345678-1234-1234-123456781234") match
         case Right(apiKey) => apiKey
         case Left(err) => throw new Exception(s"Unexpected error $err")
@@ -37,4 +70,40 @@ class SightClientImplTest extends FunSuite
         sightClientImpl2.recognize(filePaths2) match
             case Left(error) => assertEquals(error, expected2)
             case Right(_) => assertFail("Should not happen as the file does not exist")
+    }
+
+    test("SightClientImpl should make the http call and return expected result when the content is valid") {
+        val fileContentReader = fileContentReaderWith(Right(Seq("foo==")), Right(Seq(BMP)))
+        val response: String = """
+        {
+            "RecognizedText": [
+                {
+                    "Text": "Invoice",
+                    "Confidence": 0.22863210084975458,
+                    "TopLeftX": 395,
+                    "TopLeftY": 35,
+                    "TopRightX": 449,
+                    "TopRightY": 35,
+                    "BottomLeftX": 395,
+                    "BottomLeftY": 47,
+                    "BottomRightX": 449,
+                    "BottomRightY": 47
+                }
+            ]
+        }
+        """
+        val expectedResponse = 
+            val rt = RecognizedText("Invoice", 0.22863210084975458, 395, 35, 449, 35, 395, 47, 449, 47)
+            Pages(Seq(Page(error = None, fileIndex = 0, pageNumber = 1, numberOfPagesInFile = 1, recognizedText = Seq(rt))))
+        given  SttpBackend[Identity, Nothing, NothingT] = withSttpBackend(postResponse = Right(response), getResponse = Right("foo"))
+        val sightClient = new SightClientImpl(apiKey,fileContentReader)
+        val filePaths = Seq("foo/goo.bmp")
+        sightClient.recognize(filePaths) match 
+            case Right(pages) => assertEquals(pages, expectedResponse)
+            case Left(error) => assertFail(s"Unexpected error $error")
+        assertEquals(postArgs.size, 1)
+        val expectedPayload = """{"makeSentences":false,"files":[{"mimeType":"image/bmp","base64File":"foo=="}]}"""
+        val expectedAuth = "Authorization: Basic 12345678-1234-1234-123456781234"
+        val expectedUrl = "https://siftrics.com/api/sight/"
+        assertEquals(postArgs.head, (expectedUrl, expectedPayload, expectedAuth))
     }
