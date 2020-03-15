d29c259617b1be2777cc434229b79872950090f3
Ashwin Bhaskar <ashwin.bhaskar@go-jek.com>
2020-03-15 08:12:22 +0530

    Update validation

--- src/main/scala/Types.scala
+++ src/main/scala/Types.scala
@@ -6,5 +6,5 @@ import sight.models.Error._
 opaque type APIKey = String
 object APIKey
     def apply(apiKey: String): Either[Error,APIKey] = apiKey.split("-") match
-        case Array(f, s, t, fo) if (f.length == 8 && s.length == 4 && t.length == 4 && fo.length == 12) => Right(apiKey)
+        case Array(f, s, t, fo, fi) if (f.length == 8 && s.length == 4 && t.length == 4 && fo.length == 4 && fi.length == 12) => Right(apiKey)
         case _ => Left(InvalidAPIKeyFormat("Invalid key. Key should be of the form xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx"))
