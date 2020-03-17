e4b93bf332368cf56faf0915a987ca294182a96f
Ashwin Bhaskar <ashwin.bhaskar@go-jek.com>
2020-03-17 08:31:52 +0530

    Use collective extensions instead normal extensions

--- src/main/scala/Extensions.scala
+++ src/main/scala/Extensions.scala
@@ -2,7 +2,8 @@ package sight.extensions
 
 import sight.models.{Page, Error, Pages}
 
-def (ps: Pages) allText: Seq[String] = ps.pages.map(_.recognizedText.map(_.text)).flatten
+extension pageOps on (ps: Pages)
+    def allText: Seq[String] = ps.pages.map(_.recognizedText.map(_.text)).flatten
 
-def (ps: Pages) allTextWithConfidenceGreaterThan(confidence: Double) = 
-    ps.pages.map(_.recognizedText.filter(_.confidence > confidence).map(_.text)).flatten
+    def allTextWithConfidenceGreaterThan(confidence: Double) = 
+        ps.pages.map(_.recognizedText.filter(_.confidence > confidence).map(_.text)).flatten
