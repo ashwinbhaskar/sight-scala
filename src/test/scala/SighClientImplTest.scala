94324385d579f98ec265efafe273118459492bf5
Ashwin Bhaskar <ashwin.bhaskar@go-jek.com>
2020-03-18 20:40:30 +0530

    Add test for wordBoundBox=true

--- src/test/scala/SighClientImplTest.scala
+++ src/test/scala/SighClientImplTest.scala
@@ -56,7 +56,7 @@ class SightClientImplTest extends FunSuite:
             def filesToBase64(filePaths: Seq[String]): Either[Error, Seq[String]] = fileToBase64
             def fileMimeTypes(filePaths: Seq[String]): Either[Error, Seq[MimeType]] = fileMimeType
 
-    test("SightClientImpl should return the same error as FileContenReader") {
+    test("SightClientImpl should return the same error as FileContenReader - With wordLevelBoundBoxes = false") {
         val fileContentReader1 = fileContentReaderWith(Right(Seq("foo==")), Left(InvalidExtension("invalid extension")))
         val sightClientImpl1 = new SightClientImpl(apiKey, fileContentReader1)
         val filePaths1 = Seq("foo/goo.qwe")
@@ -70,6 +70,24 @@ class SightClientImplTest extends FunSuite:
         val filePaths2 = Seq("foo/goo.bmp")
         val expected2 = FileDoesNotExist("file does not exist")
         sightClientImpl2.recognize(filePaths2) match
+            case Left(error) => assertEquals(error, expected2)
+            case Right(_) => assertFail("Should not happen as the file does not exist")
+    }
+
+    test("SightClientImpl should return the same error as FileContenReader - With wordLevelBoundBoxes = true") {
+        val fileContentReader1 = fileContentReaderWith(Right(Seq("foo==")), Left(InvalidExtension("invalid extension")))
+        val sightClientImpl1 = new SightClientImpl(apiKey, fileContentReader1)
+        val filePaths1 = Seq("foo/goo.qwe")
+        val expected1 = InvalidExtension("invalid extension")
+        sightClientImpl1.recognize(filePaths1) match
+            case Left(error) => assertEquals(error, expected1)
+            case Right(_) => assertFail("Should not happen as the extension given is invalid")
+
+        val fileContentReader2 = fileContentReaderWith(Left(FileDoesNotExist("file does not exist")), Right(Seq(BMP)))
+        val sightClientImpl2 = new SightClientImpl(apiKey, fileContentReader2)
+        val filePaths2 = Seq("foo/goo.bmp")
+        val expected2 = FileDoesNotExist("file does not exist")
+        sightClientImpl2.recognize(filePaths2, true) match
             case Left(error) => assertEquals(error, expected2)
             case Right(_) => assertFail("Should not happen as the file does not exist")
     }
