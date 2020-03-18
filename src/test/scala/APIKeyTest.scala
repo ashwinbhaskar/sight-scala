2757667429cebc396b62814a287ea5134e1ceb67
Ashwin Bhaskar <ashwin.bhaskar@go-jek.com>
2020-03-18 20:40:37 +0530

    Add APIKey Test

---
+++ src/test/scala/APIKeyTest.scala
@@ -0,0 +1,19 @@
+import sight.types.APIKey
+import sight.models.Error.InvalidAPIKeyFormat
+import sight.models.Error
+import cats.implicits.{given _}
+import scala.language.implicitConversions
+
+class APIKeyTest extends munit.FunSuite:
+
+    test("Should return error when the format of the key is wrong") {
+        val expected = InvalidAPIKeyFormat("Invalid key. Key should be of the form xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx").asLeft[APIKey]
+        assertEquals(APIKey("foo"), expected)
+        assertEquals(APIKey("1234-4568"), expected)
+        assertEquals(APIKey("12345678-4568-1234-123456781234"), expected)
+    }
+    test("Should return APIKey when the format is correct") {
+        APIKey("12345678-1234-1234-1234-123456781234") match
+            case Right(_) => assert(true)
+            case Left(e) => assertFail(s"failed with $e")
+    }
